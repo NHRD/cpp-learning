be595e2a71a730bdd63bf9cf377fb6e9f18f63c1
NHRD <naohisa.harada@gmail.com>
2025-04-05 16:48:24 +0900

    4/5

---
+++ literal.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << 0b1111  << endl;
+    cout << 010     << endl;
+    cout << 10 - 1  << endl;;
+    cout << 0x10    << endl;
+    cout << 123'456 << endl;
+}
