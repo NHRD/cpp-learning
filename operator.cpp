618a95ae449523f584c2d39c744f87938903045f
NHRD <naohisa.harada@gmail.com>
2025-04-06 16:11:11 +0900

    4/6 2

---
+++ operator.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << (10 / 4) << endl;
+    cout << (1 / 3) << endl;
+    cout << (10 % 4) << endl;
+    cout << (1 + 2 * 3) << endl;
+    cout << ((1 + 2) * 3) << endl;
+    cout << (1 / 3.0) << endl;
+}
