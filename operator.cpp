de7b9fa4cb0e6a22bcb662121ac4dfc44a956636
NHRD <naohisa.harada@gmail.com>
2025-04-06 16:16:36 +0900

    4/6 3

--- operator.cpp
+++ operator.cpp
@@ -8,4 +8,10 @@ int main(){
     cout << (1 + 2 * 3) << endl;
     cout << ((1 + 2) * 3) << endl;
     cout << (1 / 3.0) << endl;
+    cout << (1 < 2 && 2 < 3) << endl;
+    cout << (1 < 2 && 3 < 2) << endl;
+    cout << (2 < 1 || 1 < 2) << endl;
+    cout << (!(2 < 1)) << endl;
+    cout << (1 < 2 && 0) << endl;
+    cout << (0 || 1 < 2) << endl;
 }
