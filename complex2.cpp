826b2950a84bd0196ba35c897ff799a90c170917
NHRD <naohisa.harada@gmail.com>
2025-04-10 20:13:12 +0900

    ４・９

--- complex2.cpp
+++ complex2.cpp
@@ -6,4 +6,10 @@ using cplx = complex<double>;
 int main(){
     cplx* p = new cplx;
     cout << *p << endl; // Output: 0.0 + 0.0
+    cplx* q = new cplx(4., 5.);
+    cout << *q << endl; // Output: 4.0 + 5.0
+    cplx* r = new cplx(*q);
+    cout << *r << endl; // Output: 4.0 + 5.0
+    cout << q-> real() << endl; // Output: 4.0
+    cout << q-> imag() << endl; // Output: 5.0
 }
