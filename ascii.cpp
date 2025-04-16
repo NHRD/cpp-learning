30525483266fe848a5d3d82c54038c8b1311dd82
NHRD <naohisa.harada@gmail.com>
2025-04-16 21:45:50 +0900

    4/16, for, while statements

---
+++ ascii.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout << " | 0 1 2 3 4 5 6 7 8 9 A B C D E F" << endl;
+    cout << "-|--------------------------------" << endl;
+    char ch = 0x20;
+    for (int i = 2; i <= 7; i++){
+        cout << i << "|";
+        for (int j = 0; j < 16; j++){
+            cout << " " << ch++;
+        }
+        cout << endl;
+    }
+}
---
+++ while.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int i = 0;
+    int j = 0;
+    while(i < 5){
+        cout << "Hello World;" << endl;
+        i++;
+    }
+    while(j < 5){
+        cout << "Goodbye World;" << endl;
+        ++j;
+    }
+}
