13190c99935720f5639de08081823cc66afcc222
NHRD <naohisa.harada@gmail.com>
2025-04-21 22:29:49 +0900

    4/21 再帰呼び出し2

---
+++ cpt5_factorial.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int factorial(int n){
+    if (n < 2) return 1;
+    return n * factorial(n - 1);
+}
+
+int main(){
+    for(int i = 1; i <= 5; ++i){
+        cout << "f(" << i << ") = " << factorial(i) << endl;
+    }
+}
