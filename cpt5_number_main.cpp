5ececff46249e9dc5b8da6b2f9084c17d1389023
NHRD <naohisa.harada@gmail.com>
2025-04-26 19:49:44 +0900

    4/26 3

---
+++ cpt5_number_main.cpp
@@ -0,0 +1,9 @@
+#include "cpt5_number.h"
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << numOfDivisors(10) << endl; // 4
+    cout << fibonacci(10) << endl; // 55
+    cout << factorial(10) << endl; // 3628800
+}
