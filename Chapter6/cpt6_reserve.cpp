dfde39b3104b66a70aaf193c4d2bdc3fb98a4e22
NHRD <naohisa.harada@gmail.com>
2025-05-01 15:25:20 +0900

    5/14

--- Chapter6/cpt6_reserve.cpp
+++ Chapter6/cpt6_reserve.cpp
@@ -4,10 +4,17 @@ using namespace std;
 
 int main() {
     vector<int> v1;
+    vector<int> v2;
+    v2.reserve(100);
     int n = 20;
     for (int i = 0; i < n; ++i) {
         cout << v1.capacity() << " ";
         v1.push_back(i);
     }
+    cout << endl;
+    for (int j = 0; j < n; ++j) {
+        cout << v2.capacity() << " ";
+        v2.push_back(i);
+        }
     cout << endl;
 }
