16fba72be45d6232c5331572a81a7dfe0ef98378
NHRD <naohisa.harada@gmail.com>
2025-05-08 21:34:05 +0900

    5/8 4

---
+++ Chapter6/cpt6_array4.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <array>
+using namespace std;
+
+void f (int* first, int* last){
+    for (int* p = first; p != last; ++p) {
+        cout << *p << ", ";
+    }
+    cout << endl;
+}
+
+int main() {
+    int a[] = {2, 3, 5, 7, 11};
+    f(begin(a), end(a));
+}
