644ea7f6dc82455d1dde7c3f8a5f104f6810f2be
NHRD <naohisa.harada@gmail.com>
2025-05-11 20:27:48 +0900

    5/11 vector

---
+++ Chapter6/cpt6_practice_test2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <unordered_map>
+#include <string>
+using namespace std;
+
+using person = pair<string, int>;
+
+int main(){
+    unordered_map<string, person> people;
+    people["John"] = {"John Doe", 30};
+    auto taro = make_pair("Taro", 32);
+    people["Taro"] = {"Taro", 100};
+
+    cout << people["Taro"].first << endl;
+}
