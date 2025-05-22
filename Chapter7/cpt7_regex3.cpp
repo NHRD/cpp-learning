f39fa4df1d75bd4c1bfcdb0974f91ce78ff889d3
NHRD <naohisa.harada@gmail.com>
2025-05-22 09:07:20 +0900

    5/22 regex, 2

---
+++ Chapter7/cpt7_regex3.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <regex>
+using namespace std;
+
+int main() {
+    string str = "My postal code is 194-0013, her postal code is 153-0042.";
+    regex rx(R"((\d{3})-(\d{4}))");
+    string tmp = "$1";
+    cout << regex_replace(str, rx, tmp) << endl;
+}
