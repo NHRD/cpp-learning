ae4c7cd67d382df166a158c728b6d736c2496a44
NHRD <naohisa.harada@gmail.com>
2025-05-22 09:00:31 +0900

    5/22 regular expression

--- Chapter7/cpt7_regex2.cpp
+++ Chapter7/cpt7_regex2.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(){
     string str = "My postal code is 194-0013, her postal code is 153-0042.";
-    regex rx(R"(\d{3} - \d{4})");
+    regex rx(R"(\d{3}-\d{4})");
     string tmp = "XXX-XXXX";
     cout << regex_replace(str, rx, tmp) << endl;
 }
