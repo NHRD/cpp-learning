a9806fe0a419310894e71a7df23ea6a7325ed3a5
NHRD <naohisa.harada@gmail.com>
2025-05-24 11:52:46 +0900

    5/23 regerx

---
+++ Chapter7/cpt7_regex5.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <regex>
+using namespace std;
+
+int main() {
+    string str = "abc, 123, xyz";
+    regex rx(R"(,|\s+)");
+    sregex_token_iterator it(str.begin(), str.end(), rx, -1);
+    sregex_token_iterator end;
+    while (it != end) {
+        cout << *it++ << endl;
+    }
+}
