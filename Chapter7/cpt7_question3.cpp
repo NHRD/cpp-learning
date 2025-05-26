0cdce3c11ea725d007c0866144c09bebeea94504
NHRD <naohisa.harada@gmail.com>
2025-05-26 15:35:43 +0900

    5/26 question 3

---
+++ Chapter7/cpt7_question3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <regex>
+using namespace std;
+
+int main(){
+    string str = "My postal code is 194-0013, her postal code is 153-0042.";
+    regex rx1(R"(\d{3}-)");
+    regex rx2(R"(\d{4})");
+    sregex_iterator it1(str.begin(), str.end(), rx1);
+    sregex_iterator it2(str.begin(), str.end(), rx2);
+    sregex_iterator end;
+    while (it1 != end && it2 != end) {
+        cout << (it1++)->str() << endl;
+        cout << (it2++)->str() << endl;
+    }
+
+}
