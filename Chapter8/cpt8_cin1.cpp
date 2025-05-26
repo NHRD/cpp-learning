6818d16584fe5ced4ddb3dcd5a3e8f682a51f783
NHRD <naohisa.harada@gmail.com>
2025-05-26 21:19:43 +0900

    5/26 chapter 8

---
+++ Chapter8/cpt8_cin1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    cout << "Input sentence and press Enter:\n";
+    string str;
+    cin >> str;
+    cout << "You entered: " << str << endl;
+}
