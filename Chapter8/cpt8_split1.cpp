156e395f3d8a7b9bb8eda1bb8c8b364151a9a0ab
NHRD <naohisa.harada@gmail.com>
2025-06-03 22:26:56 +0900

    6/3

---
+++ Chapter8/cpt8_split1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main(){
+    ifstream datafile("test.dat");
+    string label;
+    int x, y, z;
+    while (datafile >> label >> x >> y >> z) {
+        cout << label << ": " << x + y + z << endl;
+    }
+    datafile.close();
+}
