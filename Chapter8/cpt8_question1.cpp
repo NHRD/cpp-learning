6481309d98653bb99736e8abeb9cb8e692603cc1
NHRD <naohisa.harada@gmail.com>
2025-06-07 20:53:38 +0900

    6/7 2

--- Chapter8/cpt8_question1.cpp
+++ Chapter8/cpt8_question1.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main() {
     ofstream outfile("numbers.dat", ios_base::out);
     for (int i = 1; i <= 1000; ++i){
+        cout << i << endl;
         outfile << i << endl;
     }
     outfile.close();
