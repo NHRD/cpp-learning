029e05eaa2cb9a6874393a66a9be2c31305e8dd7
NHRD <naohisa.harada@gmail.com>
2025-06-30 13:33:27 +0900

    6/30

--- Chapter9/cpt9_random.cpp
+++ Chapter9/cpt9_random.cpp
@@ -4,9 +4,13 @@ using namespace std;
 
 int main() {
     random_device engine;
-uniform_int_distribution<unsigned> dist1(1, 6);
-for (int i = 0; i < 10; ++i) {
-    cout << dist1(engine) << ", ";
-}
-cout << endl;
+    uniform_int_distribution<unsigned> dist1(1, 6);
+    for (int i = 0; i < 10; ++i) {
+        cout << dist1(engine) << ", ";
+    }
+    cout << endl;
+    uniform_real_distribution<double> dist2(0.0, 1.0);
+    for (int i = 0; i < 5; ++i) {
+        cout << dist2(engine) << ", ";
+    }
 }
